208610902fdd740be57c65fe3ed24a6c62f8beff
Travis Paul <tr@vispaul.me>
2018-03-29 23:00:50 +0800

    Initial open source commit :sparkles:

---
+++ README.md
@@ -0,0 +1,181 @@
+# MySQL Query Audit Plugin
+
+This plugin is designed to log read access such as ``SELECT`` and ``JOIN`` operations against a list of configured tables. It is currently only tested against MySQL Community 5.7 on CentOS 7.
+
+This plugin was created because:
+* The [MariaDB Server Audit Plugin](https://github.com/MariaDB/server/tree/10.3/plugin/server_audit) logs too much information and doesn't have configuration options to tune it to our needs. Log file sizes quickly get out of hand.
+* [McAffee's mysql-audit plugin](https://github.com/mcafee/mysql-audit) requires [SELinux to be disabled](https://github.com/mcafee/mysql-audit/wiki/Troubleshooting#plugin-fails-to-load-with-hot-patching-problems) which is [not acceptable](https://stopdisablingselinux.com/) in our environment.
+* Oracle's Enterprise MySQL _starts_ at $5k/year and would require us to reprovision all our DB instances.
+
+## :zap: Under Active Development :volcano:
+This plugin is still under development. You should anticipate significant changes to the JSON log format and global variable names before an official release is created.
+
+Pull requests to support other operating systems or MariaDB are welcome but we may not be able to put any significant time or effort into older versions of MySQL.
+
+## Log File Format
+By default logs are written to: ``/var/log/query_audit.log``. Logs are saved as newline delimited JSON. For example:
+
+```
+{"timestamp": "2018-03-07 09:53:32", "connection_user": "user@host", "db": "dbname", "table": "tablename", "query": "... FULL QUERY ..."}
+```
+
+## Installation
+
+You can [download the latest development release from Manta](https://us-east.manta.joyent.com/tpaul/public/dev/mysql/query_audit/latest/query_audit.so) or you can follow the [Build Instructions](#build-instructions) to create your own copy.
+
+```
+# Download or copy the plugin to the server:
+curl -O https://us-east.manta.joyent.com/tpaul/public/dev/mysql/query_audit/latest/query_audit.so
+
+# Install the plugin to the mysql plugin directory:
+sudo install -g root -o root -m 0755 query_audit.so /usr/lib64/mysql/plugin
+
+# Set the SELinux context:
+sudo chcon -u system_u -r object_r -t lib_t /usr/lib64/mysql/plugin/query_audit.so
+
+# Create the log file:
+sudo touch /var/log/query_audit.log
+
+# Set owner, pemissions, and SELinux context:
+sudo chown mysql:mysql /var/log/query_audit.log
+sudo chmod 0640 /var/log/query_audit.log
+sudo chcon -u system_u -r object_r -t mysqld_log_t /var/log/query_audit.log
+
+# Install the plugin:
+echo "INSTALL PLUGIN query_audit SONAME 'query_audit.so';" | mysql -u root -p
+```
+
+If you ever need to update the plugin you must first uninstall the old version:
+
+```
+echo "UNINSTALL PLUGIN query_audit;" | mysql -u root -p
+```
+
+## Configuring the Plugin
+
+Global configuration variables for the plugin can be set at runtime using ``SET GLOBAL VAR=VALUE`` and can be made persistent by adding them to the my.cnf configuration file.
+
+You can get a list of all variables for this plugin and their values with the following command:
+
+```
+SHOW GLOBAL VARIABLES LIKE 'query_audit%';
+```
+
+## Configuration Variables
+
+### query\_audit\_log\_file
+
+This variable must contain an absolute path to the log file. By design, the plugin (and thus, MySQL) will **NOT** start if this file cannot be opened or created.
+
+**my.cnf (persistent):**
+
+```
+query_audit_log_file=/var/log/query_audit.log
+```
+
+**SQL (runtime):**
+
+```
+SET GLOBAL query_audit_log_file='/var/log/query_audit_2.log';
+```
+
+### query\_audit\_always\_fflush
+
+When this variable is set to ``on`` or ``1`` the plugin will call [fflush(3)](http://man7.org/linux/man-pages/man3/fflush.3.html) after writing to the file. This may incur a minor a performance penalty, but it ensures that user space buffers are flushed to disk.
+
+**my.cnf (persistent):**
+
+```
+query_audit_always_fflush=on
+```
+
+**SQL (runtime):**
+
+```
+SET GLOBAL query_audit_always_fflush=1;
+```
+
+### query\_audit\_log\_databases
+
+This variable should contain a comma-separated list of database names. The databases set in this variable will be monitored for read access. If there is also a matching table name in the ``query_audit_log_tables`` variable, the query will be logged.
+
+**my.cnf (persistent):**
+
+```
+query_audit_log_databases=inventory,wiki
+```
+
+**SQL (runtime):**
+
+```
+SET GLOBAL query_audit_log_databases='inventory,wiki';
+```
+
+### query\_audit\_log\_tables
+
+This variable should contain a comma-separated list of table names.  You must also configure a matching database name in the ``query_audit_log_databases`` variable.
+
+**my.cnf (persistent):**
+
+```
+query_audit_log_tables=snacks,kinds_of_cheese,things_beau_says
+```
+
+**SQL (runtime):**
+
+```
+SET GLOBAL query_audit_log_tables='snacks,kinds_of_cheese,things_beau_says';
+```
+
+## Build Instructions
+
+You need to download a copy of the MySQL Server source code, copy the query\_audit plugin source code directory into ``/path/to/mysql-source/plugin``, then build MySQL with the same configuration as the production instance. This process will produce a shared object file ([query_audit.so]((https://us-east.manta.joyent.com/tpaul/public/dev/mysql/query_audit/latest/query_audit.so))) that can be installed into a running instance.
+
+### Build dependencies
+
+
+```
+sudo yum install yum-utils rpmdevtools cmake gcc gcc-c++ bison libaio-devel numactl-devel ncurses-devel
+```
+
+### Obtaining the MySQL Source
+
+```
+sudo rpm -i http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm
+yumdownloader --nogpgcheck --source mysql-community-server
+rpmdev-extract mysql-community-5.7.*.el7.src.rpm
+cd mysql-community-5.7.*.el7.src
+rpmdev-extract mysql-5.7.*.tar.gz boost_1_*.tar.bz2
+
+```
+
+
+### Place the query audit plugin source into the MySQL source tree
+
+Copy or checkout the query audit plugin into the MySQL source tree's plugin directory:
+
+```
+cp -r /path/to/query_audit /path/to/mysql-community-5.7.xx-1.el7.src/mysql-5.7.xx/plugin
+cd /path/to/mysql-community-5.7.xx-1.el7.src/mysql-5.7.xx
+```
+
+### Building MySQL and the plugin
+
+You can now perform a build of the entire MySQL source and the plugin will be built along with it. You will need around 4GB of memory to build MySQL. When building with 2GB of memory I observed the C++ compiler being killed by the OOM killer halfway through the build.
+
+The CMake build options are taken from the spec file of mysql-community-5.7.21-1.el7.src.rpm and the file paths were altered to support a local build.
+
+```
+mkdir build
+cd build
+cmake ../ -DBUILD_CONFIG=mysql_release -DINSTALL_LAYOUT=RPM -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_BOOST=../.. -DWITH_SYSTEMD=1 -DWITH_INNODB_MEMCACHED=1 -DINSTALL_LIBDIR="/lib64/mysql" -DINSTALL_PLUGINDIR="/lib64/mysql/plugin" -DMYSQL_UNIX_ADDR="/var/lib/mysql/mysql.sock" -DMYSQLX_UNIX_ADDR="/var/run/mysqld/mysqlx.sock" -DFEATURE_SET="community" -DWITH_EMBEDDED_SERVER=1 -DWITH_EMBEDDED_SHARED_LIBRARY=1 -DWITH_NUMA=ON
+
+# this will take some time on the first run (easily > 20 mins)
+# incremental builds usually take only a few seconds
+make
+
+```
+
+The compiled shared object will be located at: ``/path/to/mysql-community-5.7.xx-1.el7.src/mysql-5.7.xx/build/plugin/query_audit/query_audit.so``
+
+You only need the ``query_audit.so`` file. It can simply be copied and installed on the server you wish to audit.
---
+++ query_audit.c
@@ -0,0 +1,363 @@
+#include <mysql/plugin.h>
+#include <mysql/plugin_audit.h>
+
+#include "cJSON.h"
+
+// How to access the pthread_mutex in mysql_mutex_t
+#define mysql_mutex_real_mutex(A) &(A)->m_mutex
+#define log_mutex_init(A,B,C) pthread_mutex_init(mysql_mutex_real_mutex(B), C)
+#define log_mutex_destroy(A) pthread_mutex_destroy(mysql_mutex_real_mutex(A))
+#define log_mutex_lock(A) pthread_mutex_lock(mysql_mutex_real_mutex(A))
+#define log_mutex_unlock(A) pthread_mutex_unlock(mysql_mutex_real_mutex(A))
+
+static PSI_mutex_key key_LOCK_operations;
+static PSI_mutex_info mutex_key_list[] = {
+    {
+        &key_LOCK_operations, "QUERY_AUDIT_plugin::lock_operations",
+        PSI_FLAG_GLOBAL
+    }
+};
+
+static mysql_mutex_t lock_operations;
+
+static char * log_file;
+static FILE * log_file_fd;
+static char log_file_default[25] = "/var/log/query_audit.log";
+static char log_file_buffer[256];
+
+static char * log_databases;
+static char * log_databases_list[32];
+static char log_databases_buffer[512];
+
+static char * log_tables;
+static char * log_tables_list[32];
+static char log_tables_buffer[512];
+
+static char always_fflush;
+
+static char empty_str[1] = { 0 };
+
+static void update_log_file(MYSQL_THD, struct st_mysql_sys_var *, void *, const void *);
+static void update_log_tables(MYSQL_THD, struct st_mysql_sys_var *, void *, const void *);
+static void update_log_databases(MYSQL_THD, struct st_mysql_sys_var *, void *, const void *);
+static void update_always_fflush(MYSQL_THD, struct st_mysql_sys_var *, void *, const void *);
+
+static MYSQL_SYSVAR_STR(log_file, log_file, PLUGIN_VAR_RQCMDARG,
+    "Path to log file.", NULL,update_log_file, log_file_default);
+
+static MYSQL_SYSVAR_STR(log_tables, log_tables, PLUGIN_VAR_RQCMDARG,
+    "Comma separated list of tables to log.", NULL, update_log_tables, NULL);
+
+static MYSQL_SYSVAR_STR(log_databases, log_databases, PLUGIN_VAR_RQCMDARG,
+    "Comma separated list of databases to log.", NULL, update_log_databases, NULL);
+
+static MYSQL_SYSVAR_BOOL(always_fflush, always_fflush, PLUGIN_VAR_OPCMDARG,
+    "Toggle calling fflush(3) after writing every log.", NULL, update_always_fflush, TRUE);
+
+static MYSQL_THDVAR_STR(connection_user,
+    PLUGIN_VAR_THDLOCAL | PLUGIN_VAR_READONLY | PLUGIN_VAR_NOSYSVAR | PLUGIN_VAR_NOCMDOPT | PLUGIN_VAR_MEMALLOC,
+    "(internal)", NULL, NULL, NULL);
+
+static struct st_mysql_sys_var * query_audit_system_variables[] = {
+    MYSQL_SYSVAR(log_file),
+    MYSQL_SYSVAR(log_tables),
+    MYSQL_SYSVAR(log_databases),
+    MYSQL_SYSVAR(always_fflush),
+    MYSQL_SYSVAR(connection_user),
+    NULL
+};
+
+static int query_audit_plugin_init(void *arg MY_ATTRIBUTE((unused))) {
+
+    fprintf(stderr, "Query Audit Plugin: Initializing.\n");
+
+    PSI_server->register_mutex("query_audit", mutex_key_list, 1);
+
+    log_mutex_init(key_LOCK_operations, &lock_operations, MY_MUTEX_INIT_FAST);
+
+    log_mutex_lock(&lock_operations);
+
+    log_file_fd = fopen(log_file, "a+");
+
+    if (log_file_fd == NULL) {
+
+        fprintf(stderr, "Query Audit Plugin: Could not create or open file '%s'.\n", log_file);
+
+        log_mutex_unlock(&lock_operations);
+
+        log_mutex_destroy(&lock_operations);
+
+        return 1;
+    }
+
+    fprintf(stderr, "Query Audit Plugin: query_audit_log_file set to '%s'.\n", log_file);
+
+    log_mutex_unlock(&lock_operations);
+
+    update_log_databases(NULL, NULL, NULL, &log_databases);
+    update_log_tables(NULL, NULL, NULL, &log_tables);
+    update_always_fflush(NULL, NULL, NULL, &always_fflush);
+    update_log_file(NULL, NULL, NULL, &log_file);
+
+    return 0;
+}
+
+static int query_audit_plugin_deinit(void *arg MY_ATTRIBUTE((unused))) {
+
+    fprintf(stderr, "Query Audit Plugin: Shutting down.\n"); // XXX Generic log fn
+
+    fflush(log_file_fd);
+
+    fclose(log_file_fd);
+
+    log_mutex_destroy(&lock_operations);
+
+    return 0;
+}
+
+static int in_list(const char * needle, char ** haystack) {
+
+    int result, i;
+    result = 0;
+    i = 0;
+    while (haystack[i] != NULL) {
+        if (strcmp(haystack[i], needle) == 0) {
+            result = 1;
+            break;
+        }
+        i++;
+    }
+
+    return result;
+}
+
+static void query_audit_write_log(const struct mysql_event_table_access *event_table, MYSQL_THD thd) {
+
+    cJSON *root;
+    char * json;
+    time_t raw_time;
+    struct tm * time_info;
+
+    // "%F %T" = "YYYY-MM-DD HH:MM:SS" (19+1 bytes)
+    char time_buffer[20];
+
+    time(&raw_time);
+    time_info = localtime (&raw_time);
+    strftime(time_buffer, 20, "%F %T", time_info);
+
+    root = cJSON_CreateObject();
+    cJSON_AddItemToObject(root, "timestamp", cJSON_CreateString(time_buffer));
+    cJSON_AddItemToObject(root, "connection_user", cJSON_CreateString(THDVAR(thd, connection_user)));
+    cJSON_AddItemToObject(root, "db", cJSON_CreateString(event_table->table_database.str));
+    cJSON_AddItemToObject(root, "table", cJSON_CreateString(event_table->table_name.str));
+    cJSON_AddItemToObject(root, "query", cJSON_CreateString(event_table->query.str));
+    json = cJSON_PrintUnformatted(root);
+
+    fprintf(log_file_fd, "%s\n", json);
+
+    free(json);
+
+    cJSON_Delete(root);
+
+    if (always_fflush) {
+        if (fflush(log_file_fd) != 0) {
+            fprintf(stderr, "Query Audit Plugin: Failed to call fflush on log_file: %s\n", log_file);
+        }
+    }
+}
+
+static int query_audit_notify(MYSQL_THD thd,
+        mysql_event_class_t event_class, const void *event) {
+
+    const struct mysql_event_connection * event_connection;
+    const struct mysql_event_table_access * event_table;
+
+    log_mutex_lock(&lock_operations);
+
+    if (event_class == MYSQL_AUDIT_CONNECTION_CLASS) {
+
+        event_connection = (const struct mysql_event_connection *) event;
+
+        if (event_connection->event_subclass == MYSQL_AUDIT_CONNECTION_CONNECT ||
+                event_connection->event_subclass == MYSQL_AUDIT_CONNECTION_CHANGE_USER) {
+
+            asprintf(&THDVAR(thd, connection_user),
+                "%s@%s",
+                event_connection->user.str,
+                (event_connection->host.str != NULL) ? event_connection->host.str : event_connection->ip.str);
+
+        }
+
+    } else if (event_class == MYSQL_AUDIT_TABLE_ACCESS_CLASS) {
+
+        event_table = (const struct mysql_event_table_access *) event;
+
+        if (event_table->event_subclass == MYSQL_AUDIT_TABLE_ACCESS_READ &&
+                in_list(event_table->table_database.str, log_databases_list) &&
+                in_list(event_table->table_name.str, log_tables_list)) {
+
+            query_audit_write_log(event_table, thd);
+        }
+    }
+
+    log_mutex_unlock(&lock_operations);
+
+    return 0;
+}
+
+static void update_always_fflush(MYSQL_THD thd __attribute__((unused)),
+        struct st_mysql_sys_var * var  __attribute__((unused)),
+        void *var_ptr  __attribute__((unused)), const void * save) {
+
+    log_mutex_lock(&lock_operations);
+
+    always_fflush = *(char *) save;
+
+    fprintf(stderr, "Query Audit Plugin: query_audit_always_fflush set to '%s'.\n",
+        (always_fflush) ? "ON" : "OFF");
+
+    log_mutex_unlock(&lock_operations);
+}
+
+static void update_log_file(MYSQL_THD thd __attribute__((unused)),
+        struct st_mysql_sys_var * var  __attribute__((unused)),
+        void *var_ptr  __attribute__((unused)), const void *save) {
+
+    char *new_log_file;
+
+    log_mutex_lock(&lock_operations);
+
+    new_log_file = (*(char **) save) ? *(char **) save : empty_str;
+
+    fprintf(stderr, "Query Audit Plugin: query_audit_log_file set to '%s'.\n", new_log_file);
+
+    strncpy(log_file_buffer, new_log_file, sizeof(log_file_buffer));
+
+    log_file_buffer[sizeof(log_file_buffer) - 1] = 0;
+
+    log_file = log_file_buffer;
+
+    fclose(log_file_fd);
+
+    log_file_fd = fopen(log_file, "a+");
+
+    if (log_file_fd == NULL) {
+        fprintf(stderr, "Query Audit Plugin: Could not create or open file '%s'.\n", log_file);
+    }
+
+    log_mutex_unlock(&lock_operations);
+}
+
+// XXX handle values > log_tables_buffer length
+static void update_log_tables(MYSQL_THD thd __attribute__((unused)),
+        struct st_mysql_sys_var * var __attribute__((unused)),
+        void *var_ptr  __attribute__((unused)), const void *save) {
+
+    char * new_tables;
+    int i;
+    char * p;
+
+    log_mutex_lock(&lock_operations);
+
+    new_tables = (*(char **) save) ? *(char **) save : empty_str;
+
+    strncpy(log_tables_buffer, new_tables, sizeof(log_tables_buffer));
+
+    log_tables_buffer[sizeof(log_tables_buffer) - 1] = 0;
+
+    log_tables = log_tables_buffer;
+
+    fprintf(stderr, "Query Audit Plugin: query_audit_log_tables set to '%s'.\n", new_tables);
+
+    i = 0;
+    while (log_tables_list[i] != NULL) {
+        free(log_tables_list[i]);
+        i++;
+    }
+
+    i = 0;
+    for (p = strtok(new_tables, ","); p != NULL; p = strtok(NULL, ",")) {
+        log_tables_list[i] = strdup(p);
+        i++;
+    }
+
+    log_tables_list[i] = NULL;
+
+    log_mutex_unlock(&lock_operations);
+
+}
+
+// XXX handle values > log_databases_buffer length
+static void update_log_databases(MYSQL_THD thd __attribute__((unused)),
+        struct st_mysql_sys_var * var __attribute__((unused)),
+        void *var_ptr  __attribute__((unused)), const void *save) {
+
+    char * new_databases;
+    int i;
+    char * p;
+
+    log_mutex_lock(&lock_operations);
+
+    new_databases = (*(char **) save) ? *(char **) save : empty_str;
+
+    strncpy(log_databases_buffer, new_databases, sizeof(log_databases_buffer));
+
+    log_databases_buffer[sizeof(log_databases_buffer) - 1] = 0;
+
+    log_databases = log_databases_buffer;
+
+    fprintf(stderr, "Query Audit Plugin: query_audit_log_databases set to '%s'.\n", new_databases);
+
+    i = 0;
+    while (log_databases_list[i] != NULL) {
+        free(log_databases_list[i]);
+        i++;
+    }
+
+    i = 0;
+    for (p = strtok(new_databases, ","); p != NULL; p = strtok(NULL, ",")) {
+        log_databases_list[i] = strdup(p);
+        i++;
+    }
+
+    log_databases_list[i] = NULL;
+
+    log_mutex_unlock(&lock_operations);
+
+}
+
+static struct st_mysql_audit query_audit_descriptor = {
+    MYSQL_AUDIT_INTERFACE_VERSION,
+    NULL,
+    query_audit_notify,
+    {
+        0,
+        (unsigned long) MYSQL_AUDIT_CONNECTION_CONNECT | MYSQL_AUDIT_CONNECTION_CHANGE_USER,
+        0,
+        0,
+        (unsigned long) MYSQL_AUDIT_TABLE_ACCESS_ALL,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+    }
+};
+
+mysql_declare_plugin(query_audit) {
+    MYSQL_AUDIT_PLUGIN,
+    &query_audit_descriptor,
+    "QUERY_AUDIT",
+    "VALID USA",
+    "Log users that query particular tables",
+    PLUGIN_LICENSE_BSD,
+    query_audit_plugin_init,
+    query_audit_plugin_deinit,
+    0x0001,
+    NULL,
+    query_audit_system_variables,
+    NULL,
+    0
+}
+mysql_declare_plugin_end;
